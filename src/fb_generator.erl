baf810373044df312184e8fa089e3da03c025532
Maksim Krzhemenevskiy <regikul@gmail.com>
2020-02-11 13:45:41 +0700

    tests again

--- src/fb_generator.erl
+++ src/fb_generator.erl
@@ -44,7 +44,10 @@ start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 init([]) ->
-  {ok, RedisConfig} = application:get_env(redis),
+  RedisConfig = case application:get_env(redis) of
+                  {ok, Config} -> Config;
+                  undefined -> []
+                end,
   Host = proplists:get_value(host, RedisConfig, ?REDIS_HOST),
   Port = proplists:get_value(port, RedisConfig, ?REDIS_PORT),
   DB = proplists:get_value(db, RedisConfig, ?REDIS_DB),
@@ -109,7 +112,7 @@ tick() ->
   timer:apply_after(1, gen_server, cast, [?SERVER, {tick}]).
 %%  gen_server:cast(?SERVER, {tick}).
 
-numbers(Upper) when Upper > 2 ->
+numbers(Upper) when Upper >= 2 ->
   [
     rand:uniform(Upper - 1) + 1,
     rand:uniform(Upper - 1) + 1,
@@ -122,9 +125,91 @@ push_into_queue(Redis, Queue, Values) ->
 -ifdef(TEST).
 
 numbers_test() ->
-  UpTo = 100,
-  N = numbers(100),
+  UpTo = 2,
+  N = numbers(UpTo),
   ?assert(length(N) =:= 3),
-  ?assert(lists:all(fun (X) -> 2 < X andalso X < UpTo end, N)).
+  ?assert(lists:all(fun (X) -> 2 =< X andalso X =< UpTo end, N)).
+
+recv_all() ->
+  receive
+    {value, Time, Value} -> [{Time, Value} | recv_all()]
+  after
+    1 -> []
+  end.
+
+-record(time_state, {
+  count :: non_neg_integer() | undefined,
+  min :: float() | undefined,
+  max :: float() | undefined,
+  first :: integer() | undefined,
+  last :: integer() | undefined,
+  prev :: integer() | undefined
+}).
+
+count_stat({Time, _Value}, #time_state{first = undefined}) ->
+  #time_state{count = 1, first = Time, prev = Time, last = Time};
+count_stat({Time, _Value}, #time_state{first = First,
+                                       count = Count,
+                                       prev = Prev,
+                                       min = undefined,
+                                       max = undefined
+                                      }) ->
+  #time_state{count = Count + 1,
+              prev = Time,
+              first = First,
+              min = Time - Prev,
+              max = Time - Prev,
+              last = Time
+             };
+count_stat({Time, _Value}, #time_state{first = First,
+                                       count = Count,
+                                       prev = Prev,
+                                       min = Min,
+                                       max = Max
+                                      }) ->
+  TDelta = Time - Prev,
+  NewMax = case TDelta > Max of
+             true -> TDelta;
+             false -> Max
+           end,
+  NewMin = case TDelta < Min of
+             true -> TDelta;
+             false -> Min
+           end,
+  #time_state{count = Count + 1,
+              prev = Time,
+              last = Time,
+              min = NewMin,
+              max = NewMax,
+              first = First
+             }.
+
+
+
+-define(RATE_PER_SEC, 3000).
+-define(RATE_DIFF, 100).
+
+rates_test() ->
+  meck:new(eredis, [passthrough]),
+  Self = self(),
+  Sender = fun (Values) ->
+             lists:foreach(fun (X) -> Self ! {value, erlang:monotonic_time(microsecond), X} end, Values)
+           end,
+  meck:expect(eredis, q, fun (_Redis, [_CMD, _KEY | Values]) -> Sender(Values) end),
+  meck:expect(eredis, start_link, fun ([_Host, _Port, _DB]) -> ok end),
+  {ok, Pid} = ?MODULE:start_link(),
+  Seconds = 3,
+  receive
+  after
+    timer:seconds(Seconds) -> gen_server:stop(Pid)
+  end,
+  Input = recv_all(),
+  TimeState = lists:foldl(fun count_stat/2, #time_state{}, Input),
+
+  ?assert(abs(TimeState#time_state.count - ?RATE_PER_SEC * Seconds) < ?RATE_DIFF),
+
+  FromMicroToMilli = 1000 * 1000,
+  Avg = TimeState#time_state.count * FromMicroToMilli / (TimeState#time_state.last - TimeState#time_state.first),
+  ?assert(abs(Avg - ?RATE_PER_SEC) < ?RATE_DIFF).
 
 -endif.
